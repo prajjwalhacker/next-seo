f58bc050cc69a168b6f5546b54b9e70a40661328
prajjwalsoni123 <prajjwalsoni123@gmail.com>
2024-11-30 20:12:56 +0530

    geerate-meta-tag

--- src/app/users2/[id]/page.jsx
+++ src/app/users2/[id]/page.jsx
@@ -16,6 +16,33 @@ export const  generateStaticParams  = async () => {
     ]
 }
 
+export async function generateMetadata({ params }){
+
+  console.log("meta data regeneration");
+  
+  const response = await fetch(`https://dummyjson.com/users/${params.id}`);
+  const user = await response.json();
+
+  return {
+    title: user.firstName,
+    description: user.lastName,
+    openGraph: {
+      title: user.firstName,
+      description: user.lastName,
+      url: `https://cosmofeed-group-picture-prod.s3-ap-south-1.amazonaws.com/my_image1732650047-2024-26-11-07-40-46.png`,
+      images: [
+        {
+          url: 'https://cosmofeed-group-picture-prod.s3-ap-south-1.amazonaws.com/my_image1732650047-2024-26-11-07-40-46.png',
+          width: 1200,
+          height: 630,
+        },
+      ],
+    },
+  };
+
+
+}
+
 async function UserPage({ params }) {
   
     const response = await fetch(`https://dummyjson.com/users/${params.id}`);
